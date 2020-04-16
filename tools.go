6b36aec070bc56a952175ae0aabf7f13956c5040
haxiwa <278614819@qq.com>
2020-04-16 12:14:14 +0800

    add functions

--- tools.go
+++ tools.go
@@ -1,8 +1,15 @@
 package tools
 
 import (
+	"encoding/csv"
 	"fmt"
+	"io"
+	"math/rand"
 	"net"
+	"os"
+	"strings"
+	"sync"
+	"time"
 )
 
 const banner = `
@@ -47,11 +54,133 @@ func Hosts(cidr string) ([]string, error) {
 }
 
 //================================================================================
+//router
+type Pool struct {
+	queue chan int
+	wg    *sync.WaitGroup
+}
+
+func NewPool(cap, total int) *Pool {
+	if cap < 1 {
+		cap = 1
+	}
+	p := &Pool{
+		queue: make(chan int, cap),
+		wg:    new(sync.WaitGroup),
+	}
+	p.wg.Add(total)
+	return p
+}
+
+func (p *Pool) AddOne() {
+	p.queue <- 1
+}
+
+func (p *Pool) DelOne() {
+	<-p.queue
+	p.wg.Done()
+}
+
 //================================================================================
+//read csv via colums(int)
+func Read_csv(path string, columns int) []string {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return nil
+	}
+	defer file.Close()
+	reader := csv.NewReader(file)
+	var list []string
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println("Error:", err)
+			return nil
+		}
+		list = append(list, record[columns])
+	}
+	return list
+}
+
 //================================================================================
+//save string to txtfile
+func Save_txt_append(content, filepath string) {
+	f, err := os.OpenFile(filepath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	defer f.Close()
+	if err != nil {
+		// 创建文件失败处理
+
+	} else {
+		_, err = f.Write([]byte(content))
+		if err != nil {
+			// 写入失败处理
+
+		}
+
+	}
+}
+func Save_txt_cover(content, filepath string) {
+	f, err := os.OpenFile(filepath, os.O_WRONLY|os.O_TRUNC, 0600)
+	defer f.Close()
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		_, err = f.Write([]byte(content))
+	}
+}
+
 //================================================================================
+func Randomstring(n int) string {
+	var letters = []byte("qwrtyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM")
+	result := make([]byte, n)
+	rand.Seed(time.Now().Unix())
+	for i := range result {
+		result[i] = letters[rand.Intn(n)]
+	}
+	return string(result)
+}
+
 //================================================================================
+//Is a digit string?
+func IsSingleDigit(data string) bool {
+	digit := []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	for _, item := range digit {
+		if data == item {
+			return true
+		}
+	}
+	return false
+}
+
+func IsDigit(data string) bool {
+	for _, item := range data {
+		if IsSingleDigit(string(item)) {
+			continue
+		} else {
+			return false
+		}
+	}
+	return true
+}
+
 //================================================================================
+//Get string between a and b in c
+func Between(str, starting, ending string) string {
+	s := strings.Index(str, starting)
+	if s < 0 {
+		return ""
+	}
+	s += len(starting)
+	e := strings.Index(str[s:], ending)
+	if e < 0 {
+		return ""
+	}
+	return str[s : s+e]
+}
+
 //================================================================================
 //================================================================================
 //================================================================================
