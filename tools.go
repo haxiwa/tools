60d282c1c4a43b9fe9fe4bb64da36b787387c5d6
haxiwa <278614819@qq.com>
2020-04-21 20:47:38 +0800

    timeout

--- tools.go
+++ tools.go
@@ -239,6 +239,20 @@ func Ip2list(addr1, addr2 string) []string {
 }
 
 //================================================================================
+func WaitTimeout(wg *sync.WaitGroup, timeout time.Duration) bool {
+	c := make(chan struct{})
+	go func() {
+		defer close(c)
+		wg.Wait()
+	}()
+	select {
+	case <-c:
+		return false // completed normally
+	case <-time.After(timeout):
+		return true // timed out
+	}
+}
+
 //================================================================================
 //================================================================================
 //================================================================================
