759171723b1e28752b6320401d88d65c4c398cf3
haxiwa <278614819@qq.com>
2020-07-16 20:34:35 +0800

    unicode

--- tools.go
+++ tools.go
@@ -377,7 +377,49 @@ func AgreeDisclaimer() {
 }
 
 //================================================================================
+func Unicode2utf8(source string) string {
+	var res = []string{""}
+	sUnicode := strings.Split(source, "\\u")
+	var context = ""
+	for _, v := range sUnicode {
+		var additional = ""
+		if len(v) < 1 {
+			continue
+		}
+		if len(v) > 4 {
+			rs := []rune(v)
+			v = string(rs[:4])
+			additional = string(rs[4:])
+		}
+		temp, err := strconv.ParseInt(v, 16, 32)
+		if err != nil {
+			context += v
+		}
+		context += fmt.Sprintf("%c", temp)
+		context += additional
+	}
+	res = append(res, context)
+	urlstr := strings.Join(res, "")
+	return urlstr
+}
+
 //================================================================================
+func Unicode2Gbk(ustr string) string {
+	sUnicodev := strings.Split(ustr, "\\u")
+	var context string
+	for _, v := range sUnicodev {
+		if len(v) < 1 {
+			continue
+		}
+		temp, err := strconv.ParseInt(v, 16, 32)
+		if err != nil {
+			panic(err)
+		}
+		context += fmt.Sprintf("%c", temp)
+	}
+	return context
+}
+
 //================================================================================
 //================================================================================
 //================================================================================
